be0102195707bdd969c52273e21eb4a536cf5e77
Davide <davidemorandini98@gmail.com>
2023-08-28 16:39:07 +0200

    js mail dadi

--- assets/JS/myscript.js
+++ assets/JS/myscript.js
@@ -51,10 +51,62 @@ checkButtonEl.addEventListener("click", function() {
     }
 });
 
-/* -  Generare un array che contenga la lista di tutti i numeri da 1 a 6
+/*  Consegna Dice Game:
+    -   In HTML:
+        -    creo un bottone (<botton></botton>) per poter inviare tale email
 
-    -  Generare un numero random da 1 a 6 sia per il giocatore sia per il computer (Math.floor( ( Math.random) scrivi un qualsiasi numero arabo)
+    -  In JS: 
+        -    Crea un array che contenga la lista di tutti i numeri da 1 a 6 (crea variabile let + array)
 
+        -   Agganciare il pulsante (<button></button>) affinché l'input possa essere trasmesso (crea variabile let + document-getElement...)
+
+        -   Mettere in ascolto il pulsante (<button></button>) affinché possa reagire ad un determinato evento in questo caso l'evento è il "click del mouse" (add.EventListsner("click", e far partire la funzione anonima function())
+
+        -   Agganciare gli elementi HTML dell'utente e del bot all'interno dei quali verrà mostrato i NUMERI scelti della scelta randomica
+
+        -   Agganciare l'elemento HTML che mostra la vincità, la perdita o il pareggio
+
+        -  Generare un numero random da 1 a 6 sia per il giocatore sia per il computer (Math.floor( ( Math.random)
+
+    If/Else:
     Stabilire il vincitore in base a chi fa il punteggio più alto (if/else)
 */
 
+
+let botChoiceElements = [1, 2, 3, 4, 5, 6 ];
+
+let playButtonEl = document.getElementById("dice_button");
+
+let botResultEl = document.getElementById("bot_result");
+
+let userResultEl = document.getElementById("user_result");
+
+let resultGameTextEl = document.getElementById("result_game_text");
+
+playButtonEl.addEventListener("click", function() {
+
+    let botNumber = Math.floor((Math.random() * 6)+1);
+    let userNumberEl = Math.floor((Math.random() * 6)+1);
+
+    if (botNumber > userNumberEl) {
+        botResultEl.innerHTML = `Il bot ha: ${botNumber + 1}`;
+
+        userResultEl.innerHTML = `Tu hai: ${userNumberEl + 1}`;
+
+        resultGameTextEl.innerHTML = "Hai perso";
+
+    } else if (botNumber < userNumberEl) {
+        botResultEl.innerHTML = `Il bot ha: ${botNumber + 1}`;    
+        
+        userResultEl.innerHTML = `Tu hai: ${userNumberEl + 1}`;
+
+        resultGameTextEl.innerHTML = "Hai vinto";
+
+    } else if (botNumber === userNumberEl) {
+        botResultEl.innerHTML = `Il bot ha: ${botNumber + 1}`;
+
+        userResultEl.innerHTML = `Tu hai: ${userNumberEl + 1}`;
+
+        resultGameTextEl.innerHTM = "Pari merito";
+    }
+})
